d4b585748af878ab7003e3bd5e1f2baa2b3e5e55
JSE955 <jordanscottedwards@gmail.com>
2024-07-27 22:38:33 -0400

    Allow tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import {loadHomePage} from './loadHome';
+import { loadAboutPage } from './loadAbout';
+import { loadMenuPage } from './loadMenu';
 
 const content = document.querySelector('#content');
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const menu = document.querySelector('#menu');
+
+home.addEventListener('click', () => {
+    content.innerHTML = "";
+    content.appendChild(loadHomePage());
+});
+
+about.addEventListener('click', () => {
+    content.innerHTML = "";
+    content.appendChild(loadAboutPage());
+});
+
+menu.addEventListener('click', () => {
+    content.innerHTML = "";
+    content.appendChild(loadMenuPage());
+});
+
 content.appendChild(loadHomePage());
